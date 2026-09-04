f4bc293ac9a6139be0fdbe071279020c53f564f1
Lutar, Stephen P. <stephenlutar2@gmail.com>
2026-09-03 21:00:45 -0400

    fix(hf): bind vertical-services writer to current source
    Signed-off-by: Stephen Lutar <stephenlutar2@gmail.com>

--- scripts/hf_publish_vertical_services.py
+++ scripts/hf_publish_vertical_services.py
@@ -24,7 +24,7 @@ from typing import Any
 from huggingface_hub import HfApi
 
 SOURCE_REPOSITORY = "szl-holdings/vertical-services"
-SOURCE_REVISION = "96c4ffa8b9a8948c9ba84dc57c0c45885feaf5de"
+SOURCE_REVISION = "dfc16a3c89e0b4bc070dc7e8ae2415e9bcb04eab"
 HF_REPOSITORY = "SZLHOLDINGS/vertical-services"
 ORIGIN = "https://szlholdings-vertical-services.hf.space"
 SOURCE_VARIABLE = "SZL_SOURCE_REVISION"
